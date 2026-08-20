a5056ad4a19c3b3085766a54e1806ef43e29ddf7
Alyson Ronnan Martins <devalyson@gmail.com>
2026-08-20 04:45:44 -0300

    feat: adcionado os comandos

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,6 @@
 # Change Log
 
-All notable changes to the "mode-column" extension will be documented in this file.
 
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
+## [0.0.1]
 
-## [Unreleased]
-
-- Initial release
+- Adcionado o modo de coluna e alterado o modo de colar
--- src/extension.ts
+++ src/extension.ts
@@ -2,18 +2,41 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+let statusBarItem: vscode.StatusBarItem;
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+	statusBarItem.command = 'modeColumn.toggle';
+	context.subscriptions.push(statusBarItem);
+
+	const toggleCmd = vscode.commands.registerCommand('modeColumn.toggle', async () => {
+		await vscode.commands.executeCommand('editor.action.toggleColumnSelection');
+		atualizarStatus();
+	});
+
+	context.subscriptions.push(toggleCmd);
+
+	context.subscriptions.push(
+		vscode.workspace.onDidChangeConfiguration((e: vscode.ConfigurationChangeEvent) => {
+			if (e.affectsConfiguration('editor.columnSelection')) {
+				atualizarStatus();
+			}
+		})
+	);
+
+	atualizarStatus();
+	statusBarItem.show();
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "mode-column" is now active!');
+	console.log('Congratulations, your extension "modeColumn" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('mode-column.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('modeColumn.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from mode column!');
@@ -22,5 +45,17 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
+function atualizarStatus(): void {
+	const ativo = vscode.workspace.getConfiguration('editor').get<boolean>('columnSelection');
+
+	if (ativo) {
+		statusBarItem.text = '$(symbol-column)';
+		statusBarItem.tooltip = 'Modo Coluna: ATIVO (Ctrl+V colará em coluna)';
+	} else {
+		statusBarItem.text = '$(symbol-column)';
+		statusBarItem.tooltip = 'Modo Coluna: INATIVO (Clique para ativar)';
+	}
+}
+
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
